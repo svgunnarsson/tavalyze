e8357694b8209bdafffef3f1914f0dcca2448859
svgunnarsson <svgunnarsson@gmail.com>
2026-07-30 17:33:12 +0000

    Add stable club slug helper

---
+++ lib/player-slugs.ts
@@ -0,0 +1,8 @@
+export function clubSlug(club: string) {
+  return club
+    .normalize("NFD")
+    .replace(/[\u0300-\u036f]/g, "")
+    .toLowerCase()
+    .replace(/[^a-z0-9]+/g, "-")
+    .replace(/^-|-$/g, "");
+}
